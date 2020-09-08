2e9b05841aa44ef54dc92e7f8c29fd65b1720d33
大牧 <1007821300@qq.com>
2020-09-08 20:35:04 +0800

    servlet3 maven archetype

---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/resources/archetype-resources/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<%--
+  Created by IntelliJ IDEA.
+  User: liumapp
+  E-mail:liumapp.com@gmail.com
+  Home-Page:http://www.liumapp.com
+  Date: 6/5/17
+  Time: 2:37 PM
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>${symbol_dollar}Title${symbol_dollar}</title>
+  </head>
+  <body>
+  ${symbol_dollar}END${symbol_dollar}
+  </body>
+</html>
---
+++ target/generated-sources/archetype/target/classes/archetype-resources/src/main/resources/archetype-resources/src/main/java/HelloServlet.java
@@ -0,0 +1,22 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+${symbol_pound}set( ${symbol_dollar}symbol_pound = '${symbol_pound}' )
+${symbol_pound}set( ${symbol_dollar}symbol_dollar = '${symbol_dollar}' )
+${symbol_pound}set( ${symbol_dollar}symbol_escape = '${symbol_escape}' )
+package ${symbol_dollar}{package};
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(value="/hello", name="helloServlet")
+public class HelloServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("Hello world!!");
+    }
+}
